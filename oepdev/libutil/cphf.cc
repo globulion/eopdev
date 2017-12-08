8448fb74770939b6e0902caca408ff7c91a7395b
Bartosz Błasiak <blasiak.bartosz@gmail.com>
2017-12-08 18:57:27 +0100

    CPHF solver added.

---
+++ oepdev/libutil/cphf.cc
@@ -0,0 +1,213 @@
+#include "cphf.h"
+
+namespace oepdev_libutil{
+
+using namespace std;
+using namespace psi;
+
+
+CPHF::CPHF(SharedWavefunction ref_wfn, Options& options) :
+            _primary(ref_wfn->basisset()),
+            _cocc(ref_wfn->Ca_subset("AO","OCC")),
+            _cvir(ref_wfn->Ca_subset("AO","VIR")),
+            _eps_occ(ref_wfn->epsilon_a_subset("AO","OCC")),
+            _eps_vir(ref_wfn->epsilon_a_subset("AO","VIR")),
+            _memory(Process::environment.get_memory()),
+            _options(options)
+{
+    _no = _eps_occ->dim();
+    _nv = _eps_vir->dim();
+    _nn = _primary->nbf();
+    _maxiter   = _options.get_int("CPHF_MAXITER");
+    _conv      = _options.get_double("CPHF_CONVERGENCE");
+    _with_diis = _options.get_bool("CPHF_DIIS");
+    _diis_dim  = _options.get_int("CPHF_DIIS_DIM");
+
+    // Initialize DIIS manager
+    if (_with_diis) {
+        for (unsigned int z=0; z<3; z++) {
+             #ifdef DIIS_BB
+               _diis.push_back(std::shared_ptr<DIIS>(new My_DIIS(_diis_dim, _no, _nv)));
+             #else
+               _diis.push_back(std::shared_ptr<DIISManager>(new DIISManager(_diis_dim, "CPHF DIIS", 
+                                 DIISManager::LargestError, DIISManager::InCore)));
+               _diis[z]->set_error_vector_size(1, DIISEntry::Matrix, 
+                          std::shared_ptr<Matrix>(new Matrix("Error Vector", _no, _nv)).get());
+               _diis[z]->set_vector_size(1, DIISEntry::Matrix, 
+                          std::shared_ptr<Matrix>(new Matrix("DIIS Vector", _no, _nv)).get());
+             #endif
+        }
+    }
+}
+
+CPHF::~CPHF() 
+{
+}
+
+void CPHF::compute(void) {
+
+    // Setup dipole perturbations //
+
+    std::vector<std::shared_ptr<Matrix>> Fmo;
+    std::vector<std::shared_ptr<Matrix>> Fao;
+
+    for (unsigned int z = 0; z < 3; z++) {
+      Fao.push_back(std::shared_ptr<Matrix>(new Matrix("F", _nn, _nn)));
+    }
+   
+    std::shared_ptr<IntegralFactory> fact(new IntegralFactory(_primary)); 
+    std::shared_ptr<OneBodyAOInt> ints(fact->ao_dipole());
+
+    ints->compute(Fao);
+
+    for (unsigned int z=0; z<3; z++) {
+      Fmo.push_back(Matrix::triplet(_cocc,Fao[z],_cvir,true,false,false));
+      Fmo[z]->scale(-2.0);
+    }
+
+    // Setup uncoupled guess //
+
+    double* eop = _eps_occ->pointer();
+    double* evp = _eps_vir->pointer();
+
+    std::vector<std::shared_ptr<Matrix>> Xmo;
+    
+    for (unsigned int z=0; z<3; z++) {
+       Xmo.push_back(std::shared_ptr<Matrix>(new Matrix("X",_no,_nv)));
+       Xmo[z]->copy(Fmo[z]);
+       double** Xp = Xmo[z]->pointer();
+       for (unsigned int i=0; i<_no; i++) {
+            for (unsigned int a=0; a<_nv; a++) {
+                 Xp[i][a] /= (evp[a] - eop[i]);
+            }
+       }
+    }
+
+    // Setup history for convergence //
+
+    std::vector<std::shared_ptr<Matrix>> Xmo_old;
+    for (unsigned int z=0; z<3; z++) {
+       Xmo_old.push_back(std::shared_ptr<Matrix>(new Matrix("X",_no,_nv)));
+    }
+ 
+    // Set JK object for 2-body part //
+
+    std::shared_ptr<JK> jk = JK::build_JK(_primary, std::shared_ptr<BasisSet>(), _options);
+    jk->set_memory(_memory / 8L); 
+    jk->initialize();
+    jk->print_header();
+    
+    std::vector<std::shared_ptr<Matrix>>& Cl = jk->C_left();
+    std::vector<std::shared_ptr<Matrix>>& Cr = jk->C_right();
+
+    const std::vector<std::shared_ptr<Matrix>>& J = jk->J();
+    const std::vector<std::shared_ptr<Matrix>>& K = jk->K();
+
+    bool converged = false;
+    double max_rms = 0.0;
+    outfile->Printf("  => CPHF Iterations <=\n\n");
+  
+    for (unsigned int iter=0; iter < _maxiter; iter++) {
+
+        // Compute generalized Fock matrices
+
+        Cl.clear();
+        Cr.clear();
+        for (unsigned int z=0; z<3; z++) {
+             Cl.push_back(_cocc);
+             Cr.push_back(Matrix::doublet(_cvir,Xmo[z],false,true));
+        }
+
+        jk->compute();
+
+        // Update amplitudes
+
+        for (unsigned int z=0; z<3; z++) {
+             Xmo[z]->copy(Fmo[z]);
+             std::shared_ptr<Matrix> J1 = Matrix::triplet(
+                 _cocc,J[z],_cvir,true,false,false);
+             std::shared_ptr<Matrix> K1 = Matrix::triplet(
+                 _cocc,K[z],_cvir,true,true ,false);
+             std::shared_ptr<Matrix> K2 = Matrix::triplet(
+                 _cocc,K[z],_cvir,true,false,false);
+             J1->scale( 4.0);
+             K1->scale(-1.0);
+             K2->scale(-1.0);
+             Xmo[z]->subtract(J1);
+             Xmo[z]->subtract(K1);
+             Xmo[z]->subtract(K2);
+             double** Xp = Xmo[z]->pointer();
+             for (unsigned int i=0; i<_no; i++) {
+                 for (unsigned int a=0; a<_nv; a++) {
+                     Xp[i][a] /= (evp[a] - eop[i]);
+                 }
+             }
+
+             // use DIIS
+             Xmo_old[z]->subtract(Xmo[z]);
+             if (_with_diis) {
+                 #ifdef DIIS_BB
+                   _diis[z]->put(Xmo_old[z], Xmo[z]);  
+                   _diis[z]->compute();
+                   _diis[z]->update(Xmo[z]); 
+                 #else
+                   _diis[z]->add_entry(2, Xmo_old[z].get(), Xmo[z].get());
+                   _diis[z]->extrapolate(1, Xmo[z].get());
+                 #endif
+              }
+        }
+
+        // Compute amplitude convergence
+
+        max_rms = 0.0;
+        for (unsigned int z=0; z<3; z++) {
+             double rms = Xmo_old[z]->rms();
+             max_rms = std::max(rms, max_rms);
+             Xmo_old[z]->copy(Xmo[z]);
+        }
+
+        outfile->Printf("  @CPHF Iter %3d: %11.3E\n", iter + 1, max_rms);
+     
+        if (max_rms < _conv) {  
+            converged = true; 
+            break;
+        }
+    } 
+
+    if (converged) {
+        outfile->Printf("\n CPHF Converged.\n\n");
+    } else {
+        outfile->Printf("\n CPHF Failed.\n\n");
+    }
+
+    // Compute and print the dipole polarizability tensor
+    
+    std::shared_ptr<Matrix> P(new Matrix("Polarizability", 3, 3));
+    double** Pp = P->pointer();
+    for (unsigned int z1=0; z1<3; z1++) {
+        for (unsigned int z2=0; z2<3; z2++) {
+            Pp[z1][z2] = Xmo[z1]->vector_dot(Fmo[z2]);
+        }
+    }
+    P->print();
+
+    _molecular_polarizability = P;
+
+}
+
+void CPHF::print(void) const {
+    outfile->Printf("  => Setup <=\n\n");
+    outfile->Printf("   Memory      = %11zu MB\n" , _memory / (1024L*1024L));
+    outfile->Printf("   Maxiter     = %11d\n"     , _maxiter);
+    outfile->Printf("   Convergence = %11.3E\n"   , _conv);
+    outfile->Printf("   Nocc        = %11d\n"     , _no);
+    outfile->Printf("   Nvir        = %11d\n"     , _nv);
+    outfile->Printf("   Nbasis      = %11d\n"     , _nn);
+    outfile->Printf("\n");
+    outfile->Printf("  => Molecule <=\n\n");
+   _primary->molecule()->print();
+    outfile->Printf("  => Primary Basis <=\n\n");
+   _primary->print();
+}
+
+}// EndNameSpace oepdev_libutil
---
+++ oepdev/libutil/cphf.h
@@ -0,0 +1,121 @@
+#include<cstdio>
+#include<string>
+#include<vector>
+
+#include "psi4/psi4-dec.h"
+#include "psi4/libparallel/parallel.h"
+#include "psi4/liboptions/liboptions.h"
+#include "psi4/libmints/vector.h"
+#include "psi4/libmints/matrix.h"
+//#include "psi4/libmints/tensor.h"
+#include "psi4/libfock/jk.h"
+#include "psi4/libmints/wavefunction.h"
+#include "psi4/libmints/basisset.h"
+#include "psi4/libmints/integral.h"
+#include "psi4/libmints/factory.h"
+#include "psi4/libciomr/libciomr.h"
+#include "psi4/libfock/jk.h"
+#include "psi4/libfock/jk_independent.h"
+#include "psi4/libfock/link.h"
+#include "psi4/libfock/direct_screening.h"
+#include "psi4/libfock/cubature.h"
+#include "psi4/libfock/points.h"
+#ifdef DIIS_BB
+  #include "diis.h"
+#else
+  #include "psi4/libdiis/diismanager.h"
+  #include "psi4/libdiis/diisentry.h"
+#endif
+
+namespace oepdev_libutil{
+
+using namespace std;
+using namespace psi;
+
+/* \brief CPHF solver class.
+ *
+ * Solves CPHF equations (now only for RHF wavefunction).
+ * Computes molecular and orbital-associated polarizabilities.
+ *
+ * Suggested usage:
+ *
+ * std::shared_ptr<CPHF> cphf(new CPHF(ref_wfn, options));
+ * cphf->compute();
+ * std::shared_ptr<Matrix> polarizability = cphf->get_molecular_polarizability();
+ * std::shared_ptr<Tensor> orbital_polars = cphf->get_orbital_polarizabilities();
+ *
+ */
+class CPHF {
+   protected:
+     /// Number of occupied orbitals
+     int _no;
+     /// Number of virtual orbitals
+     int _nv;
+     /// Number of basis functions
+     int _nn;
+     /// Memory
+     long int _memory;
+     /// Maximum number of iterations
+     int _maxiter;
+     /// CPHF convergence threshold
+     double _conv;
+     /// whether use DIIS or not
+     bool _with_diis;
+     /// Size of subspace 
+     int _diis_dim;
+     /// Primary Basis Set
+     std::shared_ptr<BasisSet> _primary;
+     /// Occupied orbitals
+     std::shared_ptr<Matrix> _cocc;
+     /// Virtual orbitals
+     std::shared_ptr<Matrix> _cvir;
+     /// Occupied orbital energies
+     std::shared_ptr<Vector> _eps_occ;
+     /// Virtual orbital energies
+     std::shared_ptr<Vector> _eps_vir;
+     /// the DIIS managers for each perturbation operator x, y and z
+     #ifdef DIIS_BB
+       std::vector<std::shared_ptr<DIIS>> _diis;
+     #else
+       std::vector<std::shared_ptr<DIISManager>> _diis;
+     #endif
+     /// Options
+     Options _options;
+
+     // <--- target quantities ---> //
+
+     /// Total (molecular) polarizability tensor
+     std::shared_ptr<Matrix> _molecular_polarizability;
+
+     /// orbital-associated polarizabilities tensors
+     //std::shared_ptr<Tensor> _orbital_polarizabilities;
+ 
+   public:
+     /// \brief Constructor
+     ///
+     /// @param ref_wfn reference HF wavefunction
+     /// @param options set of Psi4 options 
+     ///
+     CPHF(SharedWavefunction ref_wfn, Options& options);
+
+     /// Desctructor
+    ~CPHF();
+
+     /// run the calculations
+     void compute(void);
+
+     /// print to output file
+     void print(void) const;
+
+     // <--- getters ---> //
+
+     /// retrieve the molecular (total) polarizability
+     std::shared_ptr<Matrix> get_molecular_polarizability(void) const {return _molecular_polarizability;};
+
+     /// retrieve the orbital-associated polarizabilities
+     //std::shared_ptr<Tensor> get_orbital_polarizabilities(void) const {return _orbital_polarizabilities;};
+
+};
+
+
+}// EndNameSpace oepdev_libutil
