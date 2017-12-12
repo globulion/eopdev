5aba63cea96f3c66a7514ec8952d3354b3bfe7eb
Bartosz Błasiak <blasiak.bartosz@gmail.com>
2017-12-12 14:18:42 +0100

    OEPotential class: basic structure sketched

--- oepdev/liboep/oep.cc
+++ oepdev/liboep/oep.cc
@@ -1,3 +1,3 @@
 #include "oep.h"
 
-OEPotential::OEPotential(void) {}
+//OEPotential::OEPotential(void) {}
--- oepdev/liboep/oep.h
+++ oepdev/liboep/oep.h
@@ -1,3 +1,24 @@
+#ifndef OEPDEV_LIBOEP_H
+#define OEPDEV_LIBOEP_H
+
+#include<cstdio>
+#include<string>
+#include<vector>
+
+#include "psi4/libparallel/parallel.h"
+#include "psi4/liboptions/options.h"
+#include "psi4/libmints/wavefunction.h"
+#include "psi4/libmints/basisset.h"
+#include "psi4/libthce/thce.h"
+
+namespace oepdev_liboep{
+
+using namespace psi;
+using namespace std;
+using SharedWavefunction = std::shared_ptr<Wavefunction>;
+using SharedBasisSet     = std::shared_ptr<BasisSet>;
+using SharedTensor       = std::shared_ptr<Tensor>;
+
 /* \brief Generalized One-Electron Potential
  * 
  * 
@@ -5,6 +26,26 @@
 class OEPotential {
   private:
   protected:
+    /// Psi4 options
+    Options _options;
+    /// Wavefunction
+    const SharedWavefunction _wfn;
+    /// Promary Basis set
+    const SharedBasisSet _primary;
+    /// Auxiliary Basis set
+    const SharedBasisSet _auxiliary;
+
   public:
-    OEPotential(void);
+    // <--- Constructors and Destructor ---> //
+
+    /// ESP-based OEP object
+    OEPotential(SharedWavefunction wfn, Options& options);
+    /// DF-based OEP object
+    OEPotential(SharedWavefunction wfn, SharedBasisSet auxiliary, Options& options);
+
+    /// Destructor
+    virtual ~OEPotential();
 };
+
+
+} // EndNameSpace oepdev_liboep
