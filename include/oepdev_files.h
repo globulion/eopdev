5194ce8a40b320c4971ee2c7bed621b43237955d
Bartosz Błasiak <blasiak.bartosz@gmail.com>
2017-12-15 23:26:45 +0100

    Header securator definition added

--- include/oepdev_files.h
+++ include/oepdev_files.h
@@ -1,2 +1,7 @@
+#ifndef oepdev_include_oepdev_files_h
+#define oepdev_include_oepdev_files_h
+
 /// Compiling options
 #define OEPDEV_USE_PSI4_DIIS_MANAGER      0
+
+#endif // oepdev_include_oepdev_files_h
