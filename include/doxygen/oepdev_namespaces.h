b435ba5ad79eb4de227d5e4bc8cbafca63e100c8
Bartosz Błasiak <blasiak.bartosz@gmail.com>
2018-02-23 21:13:44 +0100

    Doxygen manuals separated from oepdev_files.h

---
+++ include/doxygen/oepdev_namespaces.h
@@ -0,0 +1,13 @@
+/**
+ * \namespace oepdev
+ * \brief OEPDev module namespace.
+ * 
+ * Contains:
+ */
+
+/**
+ * \namespace psi
+ * \brief Psi4 package namespace.
+ *
+ * Contains all Psi4 functionalities.
+ */
