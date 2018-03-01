5770f8e1a253128afaecb0159894f61b4acde50e
Bartosz Błasiak <blasiak.bartosz@gmail.com>
2018-03-01 14:08:15 +0100

    Example for iterators updated

--- doc/examples/prog/example_integrals_iter.cc
+++ doc/examples/prog/example_integrals_iter.cc
@@ -7,27 +7,27 @@ void iterate(std::shared_ptr<oepdev::IntegralFactory> ints)
   const double* buffer = tei->buffer();
 
   // Create iterator to go through all shell quartet combinations
-  oepdev::AllAOShellCombinationsIterator shellIter(ints);
+  oepdev::SharedShellsIterator shellIter = oepdev::ShellCombinationsIterator::build(ints, "ALL", 4);
 
   // Iterate over shells, and then over all integrals in each shell quartet
-  for (shellIter.first(); shellIter.is_done() == false; shellIter.next())
+  for (shellIter->first(); shellIter->is_done() == false; shellIter->next())
   {
        // Compute all integrals between shells in the current quartet
-       shellIter.compute_shell(tei);
+       shellIter->compute_shell(tei);
 
        // Create iterator to go through all integrals within a shell quartet
-       oepdev::AllAOIntegralsIterator intsIter(shellIter);
+       oepdev::SharedAOIntsIterator intsIter = shellIter->ao_iterator("ALL");
 
-       for (intsIter.first(); intsIter.is_done() == false; intsIter.next())
+       for (intsIter->first(); intsIter->is_done() == false; intsIter->next())
        {
             // Grab current (ij|kl) indices here
-            int i = intsIter.i();
-            int j = intsIter.j();
-            int k = intsIter.k();
-            int l = intsIter.l();
+            int i = intsIter->i();
+            int j = intsIter->j();
+            int k = intsIter->k();
+            int l = intsIter->l();
 
             // Grab the (ij|kl) integral
-            double integral = buffer[intsIter.index()];
+            double integral = buffer[intsIter->index()];
        }
   }
 }
