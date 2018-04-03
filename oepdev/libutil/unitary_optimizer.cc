d14f8d15f0c9b5640cc3cef57f8634e687b770a7
Bartosz Błasiak <blasiak.bartosz@gmail.com>
2018-04-03 20:34:57 +0200

    Revision - no changes in implementation, just turn of methods implementation changed to match header file

--- oepdev/libutil/unitary_optimizer.cc
+++ oepdev/libutil/unitary_optimizer.cc
@@ -97,16 +97,6 @@ void UnitaryOptimizer::run_(const std::string& opt)
   this->refresh_();
   this->optimize_(opt);
 }
-void UnitaryOptimizer::update_conv_()
-{
-  conv_current_ = std::abs(Znew_-Zold_);
-}
-void UnitaryOptimizer::update_iter_()
-{
-  for (int i = 0; i < n_; ++i) Xold_[i] = Xnew_[i];
-  Zold_ = Znew_;
-  niter_ += 1;
-}
 void UnitaryOptimizer::optimize_(const std::string& opt) 
 {
   if (verbose_) 
@@ -133,24 +123,33 @@ void UnitaryOptimizer::optimize_(const std::string& opt)
   }
   
 }
-double UnitaryOptimizer::eval_Z_(double* X, double* R, double* P)
+void UnitaryOptimizer::refresh_()
 {
-   double Z = 0.0;
-   for (int i = 0; i < n_; ++i) {
+   for (int i = 0; i < n_*n_; ++i) R_[i] = R0_[i];
+   for (int i = 0; i < n_   ; ++i) {
+        P_[i] = P0_[i];
+        X_[IDX(i,i,n_)] = 1.0; 
         for (int j = 0; j < n_; ++j) {
-             Z -= X[IDX(i,j,n_)] * P[j];
-             for (int k = 0; k < n_; ++k) {
-                  for (int l = 0; l < n_; ++l) {
-                       Z += X[IDX(i,j,n_)] * X[IDX(k,l,n_)] * R[IDX(j,l,n_)];
-                  }
-             }
+             if (i!=j) X_[IDX(i,j,n_)] = 0.0;
         }
    }
-   return Z;
+   std::memset(S_, 0, 4*sizeof(double));
+   // Compute initial Z value
+   this->Zinit_ = this->eval_Z_(X_, R0_, P0_);
+   this->Zold_  = this->Zinit_;
+   this->niter_ = 0;
+   this->conv_current_ = 1.0e+18;
+   this->success_ = false;
 }
-double UnitaryOptimizer::eval_Z_()
+void UnitaryOptimizer::update_conv_()
 {
-   return this->eval_Z_(X_, R0_, P0_);
+  conv_current_ = std::abs(Znew_-Zold_);
+}
+void UnitaryOptimizer::update_iter_()
+{
+  for (int i = 0; i < n_; ++i) Xold_[i] = Xnew_[i];
+  Zold_ = Znew_;
+  niter_ += 1;
 }
 void UnitaryOptimizer::update_Z_()
 {
@@ -189,38 +188,37 @@ void UnitaryOptimizer::update_RP_()
    }
    for (int i = 0; i < n_*n_; ++i) R_[i] = W_[i];
 }
-void UnitaryOptimizer::refresh_()
+void UnitaryOptimizer::update_X_()
 {
-   for (int i = 0; i < n_*n_; ++i) R_[i] = R0_[i];
-   for (int i = 0; i < n_   ; ++i) {
-        P_[i] = P0_[i];
-        X_[IDX(i,i,n_)] = 1.0; 
+   for (int i = 0; i < n_; ++i) {
         for (int j = 0; j < n_; ++j) {
-             if (i!=j) X_[IDX(i,j,n_)] = 0.0;
+             double v = 0.0;
+             for (int k = 0; k < n_; ++k) {
+                  v += Xnew_[IDX(i,k,n_)] * X_[IDX(k,j,n_)];
+             }
+             W_[IDX(i,j,n_)] = v;
         }
    }
-   std::memset(S_, 0, 4*sizeof(double));
-   // Compute initial Z value
-   this->Zinit_ = this->eval_Z_(X_, R0_, P0_);
-   this->Zold_  = this->Zinit_;
-   this->niter_ = 0;
-   this->conv_current_ = 1.0e+18;
-   this->success_ = false;
+   for (int i = 0; i < n_*n_; ++i) X_[i] = W_[i];
 }
-ABCD UnitaryOptimizer::get_ABCD_(int i, int j)
+double UnitaryOptimizer::eval_Z_(double* X, double* R, double* P)
 {
-  double A = P_[i] + P_[j];
-  double B = P_[i] - P_[j];
-  double C =-2.0 * (R_[IDX(i,j,n_)] + R_[IDX(j,i,n_)]);
-  double D = 2.0 * (R_[IDX(j,j,n_)] - R_[IDX(i,i,n_)]);
-  for (int ii = 0; ii < n_; ++ii) {
-       if ((ii != i) && (ii != j)) {
-           A -= R_[IDX(j,ii,n_)] + R_[IDX(i,ii,n_)] + R_[IDX(ii,j,n_)] + R_[IDX(ii,i,n_)];
-           B += R_[IDX(j,ii,n_)] - R_[IDX(i,ii,n_)] + R_[IDX(ii,j,n_)] - R_[IDX(ii,i,n_)];
-       }
-  }
-  ABCD abcd = {A, B, C, D};
-  return abcd;
+   double Z = 0.0;
+   for (int i = 0; i < n_; ++i) {
+        for (int j = 0; j < n_; ++j) {
+             Z -= X[IDX(i,j,n_)] * P[j];
+             for (int k = 0; k < n_; ++k) {
+                  for (int l = 0; l < n_; ++l) {
+                       Z += X[IDX(i,j,n_)] * X[IDX(k,l,n_)] * R[IDX(j,l,n_)];
+                  }
+             }
+        }
+   }
+   return Z;
+}
+double UnitaryOptimizer::eval_Z_()
+{
+   return this->eval_Z_(X_, R0_, P0_);
 }
 double UnitaryOptimizer::eval_dZ_(double g, double* R, double* P, int i, int j)
 {
@@ -240,21 +238,6 @@ double UnitaryOptimizer::eval_dZ_(double g, double* R, double* P, int i, int j)
        }
   }
   return dZ;
-}
-void UnitaryOptimizer::form_X_(int i, int j, double gamma)
-{
-  for (int ii = 0; ii < n_; ++ii) {
-       Xnew_[IDX(ii,ii,n_)] = 1.0;
-       for (int jj = 0; jj < n_; ++jj) {
-            if (ii != jj) Xnew_[IDX(ii,jj,n_)] = 0.0;
-       }
-  }
-  double c = cos(gamma);
-  double s = sin(gamma);
-  Xnew_[IDX(i,i,n_)] = c;
-  Xnew_[IDX(j,j,n_)] = c;
-  Xnew_[IDX(i,j,n_)] = s;
-  Xnew_[IDX(j,i,n_)] =-s;
 }
 double UnitaryOptimizer::eval_Z_trial_(int ii, int jj, double gamma)
 {
@@ -284,61 +267,6 @@ double UnitaryOptimizer::eval_Z_trial_(int ii, int jj, double gamma)
   }
   return Z;
 }
-double UnitaryOptimizer::func_0_(double g, const ABCD& abcd)
-{
-  double s = sin(g);
-  double c = cos(g);
-  double s2= sin(2.0*g);
-  double c2= cos(2.0*g);
-  return abcd.A * s + abcd.B * c + abcd.C * s2 + abcd.D * c2;
-}
-double UnitaryOptimizer::func_1_(double g, const ABCD& abcd)
-{
-  double s = sin(g);
-  double c = cos(g);
-  double s2= sin(2.0*g);
-  double c2= cos(2.0*g);
-  return abcd.A * c - abcd.B * s + 2.0 * abcd.C * c2 - 2.0 * abcd.D * s2;
-}
-double UnitaryOptimizer::func_2_(double g, const ABCD& abcd)
-{
-  double s = sin(g);
-  double c = cos(g);
-  double s2= sin(2.0*g);
-  double c2= cos(2.0*g);
-  return -(abcd.A * s + abcd.B * c + 4.0 * abcd.C * s2 + 4.0 * abcd.D * c2);
-}
-double UnitaryOptimizer::find_root_halley_(double x0, const ABCD& abcd)
-{
-  double xold = x0;
-  double xnew;
-  double conv = 1.0e+8;
-  int niter = 0;
-  while (conv > 1.0e-8) {
-     double f0 = func_0_(xold, abcd);
-     double f1 = func_1_(xold, abcd);
-     double f2 = func_2_(xold, abcd);
-     xnew = xold - 2.0 * f1 / (2.0*f1*f1 - f0*f2);
-     conv = std::abs(xold - xnew);
-     xold = xnew;
-     niter+= 1;
-     if (niter > 3000) break;
-  }
-  return xnew;
-}
-void UnitaryOptimizer::update_X_()
-{
-   for (int i = 0; i < n_; ++i) {
-        for (int j = 0; j < n_; ++j) {
-             double v = 0.0;
-             for (int k = 0; k < n_; ++k) {
-                  v += Xnew_[IDX(i,k,n_)] * X_[IDX(k,j,n_)];
-             }
-             W_[IDX(i,j,n_)] = v;
-        }
-   }
-   for (int i = 0; i < n_*n_; ++i) X_[i] = W_[i];
-}
 void UnitaryOptimizer::form_X0_()
 {
    for (int i = 0; i < n_; ++i) {
@@ -348,43 +276,20 @@ void UnitaryOptimizer::form_X0_()
         }
    }
 }
-bool UnitaryOptimizer::lt_(double a, double b)
+void UnitaryOptimizer::form_X_(int i, int j, double gamma)
 {
-   if (a < b) return true;
-   return false;
-}
-bool UnitaryOptimizer::gt_(double a, double b)
-{
-   if (a < b) return false;
-   return true;
-}
-double UnitaryOptimizer::find_gamma_(const ABCD& abcd, int i, int j, const std::string& opt)
-{
-   double gamma = 0.0;
-   if (opt == "min") {
-       double Zold = 1.0e+18;
-       for (int ii = 0; ii < 4; ++ii) {
-            if (this->func_1_(S_[ii], abcd) > 0.0) {
-                double Z = this->eval_Z_trial_(i, j, S_[ii]);
-                if (Z < Zold) {
-                    gamma = S_[ii];
-                    Zold = Z;
-                }
-            }
+  for (int ii = 0; ii < n_; ++ii) {
+       Xnew_[IDX(ii,ii,n_)] = 1.0;
+       for (int jj = 0; jj < n_; ++jj) {
+            if (ii != jj) Xnew_[IDX(ii,jj,n_)] = 0.0;
        }
-   } else { 
-       double Zold =-1.0e+18;
-       for (int ii = 0; ii < 4; ++ii) {
-            if (this->func_1_(S_[ii], abcd) < 0.0) {
-                double Z = this->eval_Z_trial_(i, j, S_[ii]);
-                if (Z > Zold) {
-                    gamma = S_[ii];
-                    Zold = Z;
-                }
-            }
-       }
-   }
-   return gamma;
+  }
+  double c = cos(gamma);
+  double s = sin(gamma);
+  Xnew_[IDX(i,i,n_)] = c;
+  Xnew_[IDX(j,j,n_)] = c;
+  Xnew_[IDX(i,j,n_)] = s;
+  Xnew_[IDX(j,i,n_)] =-s;
 }
 void UnitaryOptimizer::form_next_X_(const std::string& opt)
 {
@@ -408,6 +313,21 @@ void UnitaryOptimizer::form_next_X_(const std::string& opt)
        }
   }
   this->form_X_(I, J, Gamma);
+}
+ABCD UnitaryOptimizer::get_ABCD_(int i, int j)
+{
+  double A = P_[i] + P_[j];
+  double B = P_[i] - P_[j];
+  double C =-2.0 * (R_[IDX(i,j,n_)] + R_[IDX(j,i,n_)]);
+  double D = 2.0 * (R_[IDX(j,j,n_)] - R_[IDX(i,i,n_)]);
+  for (int ii = 0; ii < n_; ++ii) {
+       if ((ii != i) && (ii != j)) {
+           A -= R_[IDX(j,ii,n_)] + R_[IDX(i,ii,n_)] + R_[IDX(ii,j,n_)] + R_[IDX(ii,i,n_)];
+           B += R_[IDX(j,ii,n_)] - R_[IDX(i,ii,n_)] + R_[IDX(ii,j,n_)] - R_[IDX(ii,i,n_)];
+       }
+  }
+  ABCD abcd = {A, B, C, D};
+  return abcd;
 }
 void UnitaryOptimizer::find_roots_boyd_(const ABCD& abcd)
 {
@@ -442,7 +362,86 @@ void UnitaryOptimizer::find_roots_boyd_(const ABCD& abcd)
          S_[i] = r;
     }
 }
-
+double UnitaryOptimizer::find_root_halley_(double x0, const ABCD& abcd)
+{
+  double xold = x0;
+  double xnew;
+  double conv = 1.0e+8;
+  int niter = 0;
+  while (conv > 1.0e-8) {
+     double f0 = func_0_(xold, abcd);
+     double f1 = func_1_(xold, abcd);
+     double f2 = func_2_(xold, abcd);
+     xnew = xold - 2.0 * f1 / (2.0*f1*f1 - f0*f2);
+     conv = std::abs(xold - xnew);
+     xold = xnew;
+     niter+= 1;
+     if (niter > 3000) break;
+  }
+  return xnew;
+}
+double UnitaryOptimizer::find_gamma_(const ABCD& abcd, int i, int j, const std::string& opt)
+{
+   double gamma = 0.0;
+   if (opt == "min") {
+       double Zold = 1.0e+18;
+       for (int ii = 0; ii < 4; ++ii) {
+            if (this->func_1_(S_[ii], abcd) > 0.0) {
+                double Z = this->eval_Z_trial_(i, j, S_[ii]);
+                if (Z < Zold) {
+                    gamma = S_[ii];
+                    Zold = Z;
+                }
+            }
+       }
+   } else { 
+       double Zold =-1.0e+18;
+       for (int ii = 0; ii < 4; ++ii) {
+            if (this->func_1_(S_[ii], abcd) < 0.0) {
+                double Z = this->eval_Z_trial_(i, j, S_[ii]);
+                if (Z > Zold) {
+                    gamma = S_[ii];
+                    Zold = Z;
+                }
+            }
+       }
+   }
+   return gamma;
+}
+bool UnitaryOptimizer::lt_(double a, double b)
+{
+   if (a < b) return true;
+   return false;
+}
+bool UnitaryOptimizer::gt_(double a, double b)
+{
+   if (a < b) return false;
+   return true;
+}
+double UnitaryOptimizer::func_0_(double g, const ABCD& abcd)
+{
+  double s = sin(g);
+  double c = cos(g);
+  double s2= sin(2.0*g);
+  double c2= cos(2.0*g);
+  return abcd.A * s + abcd.B * c + abcd.C * s2 + abcd.D * c2;
+}
+double UnitaryOptimizer::func_1_(double g, const ABCD& abcd)
+{
+  double s = sin(g);
+  double c = cos(g);
+  double s2= sin(2.0*g);
+  double c2= cos(2.0*g);
+  return abcd.A * c - abcd.B * s + 2.0 * abcd.C * c2 - 2.0 * abcd.D * s2;
+}
+double UnitaryOptimizer::func_2_(double g, const ABCD& abcd)
+{
+  double s = sin(g);
+  double c = cos(g);
+  double s2= sin(2.0*g);
+  double c2= cos(2.0*g);
+  return -(abcd.A * s + abcd.B * c + 4.0 * abcd.C * s2 + 4.0 * abcd.D * c2);
+}
 std::shared_ptr<psi::Matrix> UnitaryOptimizer::psi_X_()
 {
   std::shared_ptr<psi::Matrix> X = std::make_shared<psi::Matrix>("MO-MO Transformation matrix X", n_, n_);
@@ -453,4 +452,3 @@ std::shared_ptr<psi::Matrix> UnitaryOptimizer::psi_X_()
   }
   return X;
 }
-
