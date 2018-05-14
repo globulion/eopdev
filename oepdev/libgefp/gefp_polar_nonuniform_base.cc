5b498634063ffbf90ff81bd4a1516b6e90210c95
Bartosz Błasiak <blasiak.bartosz@gmail.com>
2018-05-14 22:56:01 +0200

    Non-uniform electric field model with linear field polarization added

--- oepdev/libgefp/gefp_polar_nonuniform_base.cc
+++ oepdev/libgefp/gefp_polar_nonuniform_base.cc
@@ -21,7 +21,35 @@ oepdev::NonUniformEFieldPolarGEFactory::~NonUniformEFieldPolarGEFactory()
 // implementations of abstract methods from base
 void oepdev::NonUniformEFieldPolarGEFactory::compute_samples(void)
 {
-  // TODO
+   int nq   = options_.get_double("DMATPOL_NTEST_CHARGE");
+   double q = options_.get_double("DMATPOL_TEST_CHARGE");
+
+   for (int n=0; n<nSamples_; ++n) {
+        std::shared_ptr<psi::Matrix> charges = std::make_shared<psi::Matrix>("Q", nq, 4);
+        for (int i=0; i<nq; ++i) {
+             std::shared_ptr<psi::Vector> point = draw_random_point();
+             charges->set(i, 0, point->get(0));
+             charges->set(i, 1, point->get(1));
+             charges->set(i, 2, point->get(2));
+             charges->set(i, 3, q);
+        }
+        std::vector<std::shared_ptr<psi::Vector>> fields_n;
+        for (int o=0; o<nSites_; ++o) {
+             double x = wfn_->molecule()->x(o);
+             double y = wfn_->molecule()->y(o);
+             double z = wfn_->molecule()->z(o);
+             std::shared_ptr<psi::Vector> field = field_due_to_charges(charges, x, y, z);
+             fields_n.push_back(field);
+             cout << oepdev::string_sprintf(" Computation for N=%2d S=%2d F=[%14.4f, %14.4f, %14.4f]\n",n+1, o+1,
+                                              field->get(0), field->get(1), field->get(2));
+        }
+
+        referenceDensityMatrixSet_[n]->copy(perturbed_dmat(charges));
+        referenceDensityMatrixSet_[n]->subtract(wfn_->Da());
+
+        electricFieldSet_         .push_back(fields_n);
+   }
+ 
 }
 // abstract methods
 void oepdev::NonUniformEFieldPolarGEFactory::compute_gradient(int i, int j)
--- oepdev/libgefp/gefp_polar_nonuniform_field_1.cc
+++ oepdev/libgefp/gefp_polar_nonuniform_field_1.cc
@@ -23,9 +23,34 @@ oepdev::LinearNonUniformEFieldPolarGEFactory::~LinearNonUniformEFieldPolarGEFact
 // Implementations of abstract methods from base class
 void oepdev::LinearNonUniformEFieldPolarGEFactory::compute_gradient(int i, int j)
 {
-  // TODO
+   for (int s=0; s<nSites_; ++s) {
+   for (int z=0; z<3; ++z) {
+        int sz = 3*s + z;
+        double g = 0.0;
+        for (int n=0; n<nSamples_; ++n) {                            
+             double dij = -referenceDensityMatrixSet_[n]->get(i, j);
+             g += dij * electricFieldSet_[n][s]->get(z);
+        }
+        Gradient_->set(sz, 0, g);
+   }}
+   Gradient_->scale(2.0);
+
 }
 void oepdev::LinearNonUniformEFieldPolarGEFactory::compute_hessian(void)
 {
-  // TODO
+   double** H = Hessian_->pointer();
+   for (int s1=0; s1<nSites_; ++s1) {
+   for (int z1=0; z1<3; ++z1) {
+        int s1z1 = 3*s1 + z1;
+        for (int s2=0; s2<nSites_; ++s2) {
+        for (int z2=0; z2<3; ++z2) {
+             int s2z2 = 3*s2 + z2;
+             double v = 0.0;
+             for (int n=0; n<nSamples_; ++n) {
+                  v += electricFieldSet_[n][s1]->get(z1) * electricFieldSet_[n][s2]->get(z2);
+             }
+             H[s1z1][s2z2] = v;
+        }}
+   }}
+   Hessian_->scale(2.0);
 }
