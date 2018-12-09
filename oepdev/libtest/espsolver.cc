c8821d8ccba2d5fa1856aaf2d1f748f296138656
marta <marta.choluj@pwr.edu.pl>
2018-12-09 12:26:10 +0100

    Implementation of test for ESPSolver class

--- oepdev/libtest/espsolver.cc
+++ oepdev/libtest/espsolver.cc
@@ -1,9 +1,48 @@
 #include <iostream>
 #include "test.h"
 #include "../lib3d/esp.h"
+#include "../lib3d/space3d.h"
 
 using namespace std;
 
 double oepdev::test::Test::test_espsolver(void)
 {
+  // Reference data for MeNH2 at RHF/6-311++G** 
+  const double charge_ref[7] = { -1.043052,  0.458551,   0.372558,
+                                 0.374973,  -0.030429,  -0.103094,
+                                 -0.029507};
+
+
+
+  psi::timer_on("Test: ESPSolver   Calculation              ");
+  std::shared_ptr<ElectrostaticPotential3D> field = std::make_shared<oepdev::ElectrostaticPotential3D>(10000, 5.5, wfn_, options_);
+  field->compute();
+  std::shared_ptr<ESPSolver> esp = std::make_shared<oepdev::ESPSolver>(field);
+  esp->compute();
+  psi::timer_off("Test: ESPSolver   Calculation              ");
+  
+
+  esp->charges()->print();
+
+  // Accumulate errors
+  double r_sum = 0.0;
+  double** c = esp->charges()->pointer();
+  const double*  r = charge_ref;
+
+  
+  for (int i=0; i<7; ++i) {
+       r_sum += pow(c[0][i] - *r, 2.0);
+       r++;
+  }
+  r_sum = sqrt(r_sum);
+
+  // Print
+  std::cout << std::fixed;
+  std::cout.precision(8);
+  std::cout << " Test result= " << r_sum << std::endl;
+
+  // Return
+  return r_sum;
+
+
 }
