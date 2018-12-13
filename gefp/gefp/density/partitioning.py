f2fcb13d318e695bef54a75506a5d9a0f22ba491
globulion <blasiak.bartosz@gmail.com>
2018-12-13 19:33:10 +0100

    GEFP Python package initialized

---
+++ gefp/gefp/density/partitioning.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#*-* coding: utf-8 *-*
+
+import psi4
+import oepdev
+import numpy
+
+class DensityDecomposition:
+    """
+ Density-Based Decomposition Scheme from Mandado and Hermida-Ramon
+ JCTC 2011
+"""
+    def __init__(self, system):
+        self.system = system
+    def compute(self): 
+        pass
---
+++ gefp/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='GEFP-OEP',
+      version='1.0.0',
+      description='Generalized Effective Fragment Potentials (GEFP) derived from One-Electron Potential (OEP) Method',
+      author='Bartosz Błasiak',
+      author_email='blasiak.bartosz@gmail.com',
+      url='',
+      packages=['gefp'],
+     )
