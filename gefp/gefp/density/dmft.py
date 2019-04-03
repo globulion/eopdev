DMFT NC works well now for HF functional. Gradients and class structure correct.

gefp/gefp/density/dmft.py
@@ -18,7 +18,7 @@ from .partitioning import Density
 
 __all__ = ["DMFT"]
 
-def aaa(a, mu):#OK
+def aaa(a, mu):
     "Projected occupation numbers"
     a_ = a.copy();
     for i in range(len(a)):
@@ -27,7 +27,8 @@ def aaa(a, mu):#OK
         elif u >= 1.0: a_[i] = 1.0
         else: a_[i] = u
     return a_
-def bbb(b, nu):#OK
+
+def bbb(b, nu):
     "Projected occupation numbers"
     b_ = b.copy();
     for i in range(len(b)):
@@ -36,7 +37,8 @@ def bbb(b, nu):#OK
         elif u >= 1.0: b_[i] = 1.0
         else: b_[i] = u
     return b_
-def find_mu(n, np):#OK
+
+def find_mu(n, np):
     "Search for mu"
     mu = 0.0
     def obj(mu, x):
@@ -46,7 +48,8 @@ def find_mu(n, np):#OK
     R = scipy.optimize.minimize(obj, mu, args=(n,))
     mu = R.x
     return mu
-def find_nu(n, np):#OK
+
+def find_nu(n, np):
     "Search for mu"
     nu = 0.0
     def obj(nu, x):
@@ -56,6 +59,7 @@ def find_nu(n, np):#OK
     R = scipy.optimize.minimize(obj, nu, args=(n,))
     nu = R.x
     return nu
+
 def density_matrix_projection(n, c, S, np, type='d'):#OK
     "Find n_new and C_new such that new density matrix is N-representable"
     if type.lower() == 'd':
@@ -105,21 +109,42 @@ class DMFT(ABC):
 
     def __init__(self, wfn, xc_functional, v_ext=default_v_ext, guess=default_guess):
         "Initialize"
-        # protected variable namespace
-        self._current_energy           = None       # Total energy
-        self._current_orbitals         = None       # Natural orbitals
-        self._current_occupancies      = None       # Natural orbital occupation numbers
-        self._current_density          = None       # 1-particle density matrix in AO basis
+        # Protected variable namespace
+        self._current_energy           = None       # Total Energy
+        self._current_orbitals         = None       # Natural Orbitals
+        self._current_occupancies      = None       # Natural Orbital Occupation Numbers
+        self._current_density          = None       # 1-Particle Density Matrix in AO Basis
 
-        # initialize all variables
+        self._xc_functional            = None       # XC Functional Object
+        self._mol                      = None       # Molecule Object
+        self._wfn                      = None       # Wavefunction Object
+        self._bfs                      = None       # Basis Set Object
+        self._bfs_name                 = None       # Basis Set Abbreviation
+        self._mints                    = None       # Integral Calculator Objects
+        self._jk                       = None       # JK Object
+        self._e_nuc                    = None       # Nuclear Repulsion Energy
+        self._np                       = None       # Number of Electron Pairs
+        self._Ca                       = None       # AO-MO Matrix at Hartree-Fock Level (LCAO-MO)
+        self._S                        = None       # AO Overlap Matrix
+        self._X                        = None       # AO Orthogonalizer 
+        self._Y                        = None       # AO Deorthogonalizer
+        self._H                        = None       # AO Core Hamiltonian Matrix
+        self._V_ext                    = None       # AO External Potential Matrix
+
+        # Initialize all variables
         self.__common_init(wfn, xc_functional, v_ext, guess)
 
-        # check if XC functional and algorithm are properly chosen
-        if self.xc_functional.abbr.lower() != 'hf' and self.abbr.lower() == 'dmft-projd':
-           print(""" Warning! The D-set is not Lipshitz with %s functional. 
+        # Sanity checks
+        if self._xc_functional.abbr.lower() != 'hf' and self.abbr.lower() == 'dmft-projd':
+           print("""\
+ Warning! The D-set is not Lipshitz with %s functional. 
  This will probably result in lack of convergence. Use P-set instead.""" % xc_functional.abbr.upper())
 
+        # Call constructor for meta class
         super(DMFT, self).__init__()
+
+
+    # ---- Public Interface ----- #
 
     @classmethod
     def create(cls, wfn, xc_functional = default_xc_functional, 
@@ -140,13 +165,18 @@ class DMFT(ABC):
                   maxit   = default_maxiter    , 
                   verbose = default_verbose_run, 
                   g_0     = default_g0         , **kwargs):
-        "Run the DMFT calculations"
+        """\
+ Run the DMFT calculations.
+"""
 
-        if verbose: print(" Running %s:%s/%s" % (self.abbr, self.xc_functional.abbr, self._basis_name))
+        if verbose: print(" Running %s:%s/%s" % (self.abbr, self._xc_functional.abbr, self._bfs_name))
 
         # Run!
         success = self._run_dmft(conv, maxit, verbose, g_0, **kwargs)
         return success
+
+
+    # ---- Public Interface: Properties ----- #
 
     @property
     def E(self): 
@@ -244,7 +274,8 @@ class DMFT(ABC):
         return success
 
     def _compute_no_exchange_energy(self):
-        E_N = self.e_nuc
+        "Compute Total Energy without Exchange-Correlation Energy"
+        E_N = self._e_nuc
         E_1 = self._compute_hcore_energy()
         E_H = self._compute_hartree_energy()
         E   = E_N + E_1 + E_H
@@ -255,19 +286,20 @@ class DMFT(ABC):
     def _compute_no_exchange_gradient_P(self):#TODO
         return NotImplementedError
     def _compute_no_exchange_gradient_nc(self, n, c):
+        "Compute Gradient excluding the Exchange-Correlation Part"
         grad_n = self.__grad_n_no_exchange(n, c)
         grad_c = self.__grad_c_no_exchange(n, c)
         grad = numpy.hstack([grad_n, grad_c.ravel()])
         return grad
                                                                            
-    def _compute_hcore_energy(self):#OK
+    def _compute_hcore_energy(self):
         "1-Electron Energy"
         H = self._H.copy()
         D = self._current_density.matrix()
         E = 2.0 * self._current_density.compute_1el_energy(D, H)
         return E
                                                                            
-    def _compute_hartree_energy(self):#OK
+    def _compute_hartree_energy(self):
         "2-Electron Energy: Hartree"
         D = self._current_density.matrix()
         E = 2.0 * self._current_density.compute_2el_energy(D, D, type='j')
@@ -295,25 +327,40 @@ class DMFT(ABC):
         pass
 
     @abstractmethod
-    def _step(self, x1, x2):
-        "Steepest-descents step"
-        pass
-
-    @abstractmethod
     def _step_0(self, x0, g0):
         "Steepest-descents initial step"
         pass
 
+    @abstractmethod
+    def _step(self, x1, x2):
+        "Steepest-descents further step"
+        pass
 
+
+    def _pack(self, n, c):
+        "Pack the n and c parameters into one hypervector"
+        x   = numpy.hstack([n, c.ravel()])
+        return x
+
+
+    def _unpack(self, x):
+        "Unpack n and c parameters from the hypervector"
+        dim = self._bfs.nbf()
+        n   = x[:dim]
+        c   = x[dim:].reshape(dim,dim)
+        return n, c
+
+
+    # --- Private Interface --- #
 
     def __common_init(self, wfn, xc_functional, V_ext, guess):#OK--->JK!!!
         "Initialize"
         # Wavefunction
         self._wfn = wfn
         # Basis set name
-        self._basis_name = wfn.basisset().name
+        self._bfs_name = wfn.basisset().name
         # XC functional
-        self.xc_functional = xc_functional
+        self._xc_functional = xc_functional
 
         # Molecule                                                                             
         self._mol = self._wfn.molecule()
@@ -325,10 +372,10 @@ class DMFT(ABC):
         self._jk = psi4.core.JK.build(self._bfs, jk_type="Direct")
         self._jk.set_memory(int(5e8))
         self._jk.initialize()
-        self.xc_functional.set_jk(self._jk)
-        self.xc_functional.set_wfn(self._wfn)
+        self._xc_functional.set_jk(self._jk)
+        self._xc_functional.set_wfn(self._wfn)
         # Nuclear repulsion energy
-        self.e_nuc = self._mol.nuclear_repulsion_energy()
+        self._e_nuc = self._mol.nuclear_repulsion_energy()
         # Number of electron pairs
         self._np = self._wfn.nalpha()
                                                                                                
@@ -336,9 +383,10 @@ class DMFT(ABC):
         # SCF LCAO-MO coefficients
         self._Ca= self._wfn.Ca_subset("AO","ALL").to_array(dense=True)
         # Overlap integrals and orthogonalizer
-        self._S = self._wfn.S().to_array(dense=True) #numpy.asarray( self._mints.ao_overlap() )
-        self._X = Density.orthogonalizer(self._S)
-        self._Y = numpy.linalg.inv(self._X)
+        self._S = self._wfn.S().to_array(dense=True)
+        self._X = Density.  orthogonalizer(self._S)
+        self._Y = Density.deorthogonalizer(self._S)
+        #self._Y = numpy.linalg.inv(self._X)
         # Hcore matrix
         self._H = self._wfn.H().to_array(dense=True)
         # External potential
@@ -356,7 +404,7 @@ class DMFT(ABC):
            e, c = numpy.linalg.eigh(numpy.linalg.multi_dot([self._X, self._H, self._X]))
            c = c[:,::-1]
            c = numpy.dot(self._X, c)
-           E = 2.0*e.sum() + self.e_nuc
+           E = 2.0*e.sum() + self._e_nuc
            D = numpy.zeros((self._bfs.nbf(), self._bfs.nbf()), numpy.float64)
            for i in range(self._np):
                D += numpy.outer(c[:,i], c[:,i])
@@ -370,16 +418,6 @@ class DMFT(ABC):
                                        order='descending', no_cutoff=0.0, renormalize=False, return_ao_orthogonal=False)
         return
 
-
-    # ----- 
-    def _unpack(self, x):#OK
-        dim = self._bfs.nbf()
-        n   = x[:dim]
-        c   = x[dim:].reshape(dim,dim)
-        return n, c
-    def _pack(self, n, c):#OK
-        x   = numpy.hstack([n, c.ravel()])
-        return x
     def __grad_n_no_exchange(self, n, c):
         "Energy gradient wrt NO occupation numbers"                   
         nn = len(n)
@@ -389,7 +427,6 @@ class DMFT(ABC):
         grad = 2.0 * Hmm
                                                                      
         # 2-electron contribution
-        #D = numpy.dot(c, numpy.dot(2.0*numpy.diag(n), c.T))
         I = numpy.identity(nn, numpy.float64)
                                                                      
         # J-type
@@ -403,6 +440,7 @@ class DMFT(ABC):
         # K-type
         # ---> moved to XC_Functional
         return grad
+
     def __grad_c_no_exchange(self, n, c):
         "Energy gradient wrt LCAO-NO wavefunction coefficients"
         nn = len(n)
@@ -437,7 +475,6 @@ class DMFT(ABC):
 
 
 
-
 class DMFT_NC(DMFT):
     def __init__(self, wfn, xc_functional, v_ext, guess):
         super(DMFT_NC, self).__init__(wfn, xc_functional, v_ext, guess)
@@ -451,23 +488,23 @@ class DMFT_NC(DMFT):
     def abbr(self): return "DMFT-NC"
 
 
-    # --- Implementation (Protected) --- #
+    # --- Implementation (Protected Interface) --- #
 
 
-    def _minimizer(self, x):#OK
-        "Minimizer function"
+    def _minimizer(self, x):
+        "Minimizer function: Total Energy"
         n, c = self._unpack(x)
         E_H  = self._compute_no_exchange_energy()
-        E_XC = self.xc_functional.energy(n, c)
+        E_XC = self._xc_functional.energy(n, c)
         E    = E_H + E_XC
         return E
 
-    def _guess(self):#OK
+    def _guess(self):
         "Initial guess"
         x = self._pack(self._current_occupancies, self._current_orbitals) 
         return x
 
-    def _density(self, x):#OK
+    def _density(self, x):
         "1-particle density matrix in AO basis"
         n, c = self._unpack(x)
         n, c = density_matrix_projection(n, c, self._S, self._np, type='d')
@@ -482,7 +519,7 @@ class DMFT_NC(DMFT):
         "Gradient"
         n, c = self._unpack(x)
         dE_n   , dE_c    = self._unpack(self._compute_no_exchange_gradient_nc(n, c))
-        dE_n_xc, dE_c_xc = self._unpack(self.xc_functional.gradient_nc(n, c))
+        dE_n_xc, dE_c_xc = self._unpack(self._xc_functional      .gradient_nc(n, c))
         dE_c_xc = numpy.dot(self._X, dE_c_xc) # OAO basis
         dE_n += dE_n_xc
         dE_c += dE_c_xc
@@ -512,10 +549,10 @@ class DMFT_NC(DMFT):
 
         # transform back to AO basis
         n, c_ = self._unpack(x_new)
-        x_new = self._pack(n, numpy.dot(self._Y, c_))
+        x_new = self._pack(n, numpy.dot(self._X, c_))
         return x_new
 
-    def _step_0(self, x0, g0):#OK
+    def _step_0(self, x0, g0):
         "Steepest-descents step. Back-transforms to OAO basis for simplicity"
         n2, c2 = self._unpack(x0)
 
@@ -527,12 +564,10 @@ class DMFT_NC(DMFT):
         gradient_2 = self._gradient(self._pack(n2, c2))
         x_new = x_old_2 - g0 * gradient_2
         
-
         # transform back to AO basis
         n, c_ = self._unpack(x_new)
-        x_new = self._pack(n, numpy.dot(self._Y, c_))
+        x_new = self._pack(n, numpy.dot(self._X, c_))
         return x_new
-
 
 
 
@@ -560,5 +595,3 @@ class DMFT_ProjP(DMFT):
 
     @property
     def abbr(self): return "DMFT-ProjP"
-
-