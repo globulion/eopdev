4c5cfb448154cfbe3b3704fc535850d1514f1f7f
globulion <blasiak.bartosz@gmail.com>
2019-11-06 18:42:00 +0100

    EFP2 induction energy implemented for closed shells and working well

--- gefp/gefp/energy/efp2.py
+++ gefp/gefp/energy/efp2.py
@@ -29,7 +29,7 @@ def electric_field(cphf, r):
     f = numpy.zeros(3)
 
     # prepare
-    D = cphf.wfn().Da().to_array(dense=True)
+    D = cphf.wfn().Da().to_array(dense=True) * 2.0
     mints = psi4.core.MintsHelper(cphf.wfn().basisset())
     mol = cphf.wfn().molecule()
 
@@ -56,60 +56,71 @@ def electric_field(cphf, r):
 
 
 
-def induction(*cphfs):
-    "Compute EFP2 induction energy from list of CPHF objects."
+def induction(*cphfs, return_all=False):
+    """
+ Compute EFP2 induction energy from list of CPHF objects. 
+
+ Returns:
+   (return_all = True) induction energy, induced dipole moments vector, electric field vector
+   (return_all = False) induction energy
+"""
+
     n_frag = len(cphfs)
+
     # extract LMO centroids, distributed polarizabilities and other data
     lmoc = []
     dpol = []
-    nmol = [x.ndocc() for x in cphfs]
+    nmos = [x.nocc() for x in cphfs]
 
     for cphf in cphfs:
-        lmoc_cphf = [cphf.lmo_centroid(x) for x in range(cphf.ndocc())]
+        lmoc_cphf = [cphf.lmo_centroid(x) for x in range(cphf.nocc())]
         lmoc.append(lmoc_cphf)
-        dpol_cphf = [cphf.polarizability(x) for x in range(cphf.ndocc())]
+        dpol_cphf = [cphf.polarizability(x) for x in range(cphf.nocc())]
         dpol.append(dpol_cphf)
 
     # allocate D and F matrices
-    DIM = sum(DIM)
+    DIM = sum(nmos)
     D = numpy.zeros((3*DIM, 3*DIM))
     F = numpy.zeros( 3*DIM)
 
     # compute D and F matrices
     i_pol = 0
     for n in range(n_frag):
-        N   = nmol[n]  # number of LMOc's
+        N   = nmos[n]  # number of LMOc's
         An  = dpol[n]  # list of distributed polarizabilities
         Ln  = lmoc[n]  # list of LMOc's
 
         i_pol += N
         for a in range(N):  
-            ix3 = 3*(i_pol - N) + 3*(a - 1)
-            D[ix3:ix3+3,ix3:ix3+3] = numpy.linalg.inv(An[a].to_array(dense=True))
+            ix3 = 3*(i_pol - N) + 3*a
+            #print(n,a,ix3, ix3+3)
 
-            ra = La[a].to_array(dense=True)
+            D[ix3:ix3+3,ix3:ix3+3] = numpy.linalg.inv(An[a].to_array(dense=True))  # diagonal elements of D matrix
+
+            ra = Ln[a].to_array(dense=True)
 
             j_pol = 0
             for m in range(n_frag):
+              M = nmos[m]
+              j_pol += M
+
+              # consider other fragments than n-th fragment
               if (n!=m):
-                M = nmol[m]
-                LM = lmoc[m]
+                Lm = lmoc[m]
 
-                rb = Lb[b].to_array(dense=True)
+                F[ix3:ix3+3] += electric_field(cphfs[m], ra)
 
-                j_pol += M
                 for b in range(M):
-                    jx3 = 3*(j_pol - M) + 3*(b - 1)
+                    rb = Lm[b].to_array(dense=True)
+                    jx3 = 3*(j_pol - M) + 3*b
                     Tab = t_2(ra, rb)
 
                     D[ix3:ix3+3,jx3:jx3+3] =-Tab
-
                     #
-                    F[ix3:ix3+3] += electric_field(cphfs[b], ra)
                 
-            
     # compute induction energy
-    P = F.T @ numpy.linalg.inv(D)
-    e =-0.5 * P.dot(F)
+    P = numpy.linalg.inv(D) @ F
+    e =-0.5 * F @ P
 
-    return e
+    if return_all: return e, P, F
+    else: return e
