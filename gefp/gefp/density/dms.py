0296a7de27e9d30f03ce51148d65ad1ff794a4cf
globulion <blasiak.bartosz@gmail.com>
2020-02-25 08:37:19 +0100

    Susceptibility 21 added

--- gefp/gefp/density/dms.py
+++ gefp/gefp/density/dms.py
@@ -19,7 +19,7 @@ from gefp.math.matrix import move_atom_rotate_molecule, rotate_ao_matrix, matrix
 __all__ = ["DMSFit", "DMS"]
 
 PSI4_DRIVER = psi4.energy
-
+numpy.random.seed(0)
 
 class DMS(ABC):
   """
@@ -56,8 +56,9 @@ class DMS(ABC):
 
   @classmethod
   def create(cls, dms_type='da', order_type='basic'):#OK
-      if   order_type.lower() == 'basicct': return BasicCT_DMS(dms_type)
-      elif order_type.lower() == 'basic'  : return Basic_DMS(dms_type)
+      if   order_type.lower() == 'basic': return       Basic_DMS(dms_type)
+      elif order_type.lower() == 'ct-1' : return    LinearCT_DMS(dms_type)
+      elif order_type.lower() == 'ct-2' : return QuadraticCT_DMS(dms_type)
       else:
          raise NotImplementedError
       
@@ -182,7 +183,7 @@ class DMS(ABC):
           B[:,:,:,1,0] = xy.copy()
           B[:,:,:,2,0] = xz.copy()
           B[:,:,:,2,1] = yz.copy()
-          self._B[(1,1)] = B
+          self._B[(2,1)] = B
 
 
 
@@ -229,7 +230,7 @@ class Basic_DMS(DMS):
       self._available_orders = [(1,0), (2,0), (0,1)]
 
 
-class BasicCT_DMS(DMS):
+class LinearCT_DMS(DMS):
   """
  Basic model of DMS that handles induction up to second-order,
  first-order pure Pauli effects and first-order pure CT effects.
@@ -251,7 +252,7 @@ class BasicCT_DMS(DMS):
 
   # --> Implementation <-- #
 
-class CT_DMS(DMS):
+class QuadraticCT_DMS(DMS):
   """
  Basic model of DMS that handles induction up to second-order,
  first-order pure Pauli effects and second-order pure CT effects.
@@ -440,10 +441,16 @@ class Translation_DMSFit(DMSFit):
       self._F_B_set = []
       self._W_AB_set= []
       self._W_BA_set= []
+      self._w_AB_set= []
+      self._w_BA_set= []
      #self._Wf_AB_set= []
      #self._Wf_BA_set= []
       self._A_AB_set= []
       self._A_BA_set= []
+      self._a_AB_set= []
+      self._a_BA_set= []
+      self._k_set   = []
+      self._l_set   = []
      #self._Af_AB_set= []
      #self._Af_BA_set= []
      #self._Aff_AB_set= []
@@ -451,6 +458,7 @@ class Translation_DMSFit(DMSFit):
       self._K_set   = []
       self._L_set   = []
       self._DIP_set = []      
+      self._H_AB_set_ref = []
 
 
   # ---> Implementation <--- #
@@ -522,7 +530,8 @@ class Translation_DMSFit(DMSFit):
           self._determine_perturbing_densities()
 
           S_AB = self._dimer_wfn.S().to_array(dense=True)[:self._nbf,self._nbf:]
-                                                                                                         
+          H_AB = self._dimer_wfn.H().to_array(dense=True)[:self._nbf,self._nbf:]
+                                                                                                        
           dD     = self._dimer_wfn.Da().to_array(dense=True)
           dD[:self._nbf,:self._nbf]-= self._D0
           dD[self._nbf:,self._nbf:]-= self._D0
@@ -538,11 +547,17 @@ class Translation_DMSFit(DMSFit):
           W_AB = self._DA @ S_AB
           W_BA = self._DB @ S_AB.T
 
+         #w_AB = self._DA @ S_AB    @ S_AB.T @ S_AB
+         #w_BA = self._DB @ S_AB.T  @ S_AB   @ S_AB.T
+
          #Wf_AB= numpy.einsum("ixab,bc->acix",F_A_mat, W_AB) # i.e. F_A_mat[:,:,i,x] @ W_AB 
          #Wf_BA= numpy.einsum("ixab,bc->acix",F_B_mat, W_BA) # i.e. F_B_mat[:,:,i,x] @ W_BA 
 
           A_AB = W_AB.T @ W_AB
           A_BA = W_BA.T @ W_BA
+
+         #a_AB = w_AB.T @ w_AB
+         #a_BA = w_BA.T @ w_BA
 
          #Af_AB= numpy.einsum("acix,ad->cdix",Wf_AB, W_AB)
          #Af_BA= numpy.einsum("acix,ad->cdix",Wf_BA, W_BA)
@@ -553,7 +568,11 @@ class Translation_DMSFit(DMSFit):
           K    = W_AB.T @ dD_AB
           L    = W_BA.T @ dD_AB.T
 
+         #k    = w_AB.T @ dD_AB
+         #l    = w_BA.T @ dD_AB.T
+
           self._S_AB_set.append(S_AB.copy())
+          self._H_AB_set_ref.append(H_AB.copy())
           self._W_AB_set.append(W_AB)
           self._W_BA_set.append(W_BA)
           self._A_AB_set.append(A_AB)
@@ -571,6 +590,14 @@ class Translation_DMSFit(DMSFit):
 
           self._K_set.append(K)
           self._L_set.append(L)
+
+         #self._k_set.append(k)
+         #self._l_set.append(l)
+         #self._w_AB_set.append(w_AB)
+         #self._w_BA_set.append(w_BA)
+         #self._a_AB_set.append(a_AB)
+         #self._a_BA_set.append(a_BA)
+
 
           self._dD_AA_set_ref.append(dD_AA)
           self._dD_BB_set_ref.append(dD_BB)
@@ -597,6 +624,16 @@ class Translation_DMSFit(DMSFit):
      #self._Aff_BA_set= numpy.array(self._Aff_BA_set)
       self._K_set   = numpy.array(self._K_set)
       self._L_set   = numpy.array(self._L_set)
+
+
+     #self._w_AB_set= numpy.array(self._w_AB_set)
+     #self._w_BA_set= numpy.array(self._w_BA_set)
+     #self._a_AB_set= numpy.array(self._a_AB_set)
+     #self._a_BA_set= numpy.array(self._a_BA_set)
+     #self._k_set   = numpy.array(self._k_set)
+     #self._l_set   = numpy.array(self._l_set)
+
+      self._H_AB_set_ref = numpy.array(self._H_AB_set_ref)
       pass
 
   def _compute_group_1(self):#OK
@@ -668,13 +705,33 @@ class Translation_DMSFit(DMSFit):
           #
           gw = numpy.einsum("nab,niu,niw->abiuw",self._K_set, self._F_A_set, self._F_A_set)
           gw+= numpy.einsum("nab,niu,niw->abiuw",self._L_set, self._F_B_set, self._F_B_set)
+          #
+          u = numpy.zeros((self._dms.n,self._dms.n,self._dms.N,6))
+          #
+          u[:,:,:,0] = gw[:,:,:,0,0].copy()       # xx
+          u[:,:,:,1] = gw[:,:,:,0,1].copy() * 2.0 # xy
+          u[:,:,:,2] = gw[:,:,:,0,2].copy() * 2.0 # xz
+          u[:,:,:,3] = gw[:,:,:,1,1].copy()       # yy
+          u[:,:,:,4] = gw[:,:,:,1,2].copy() * 2.0 # yz
+          u[:,:,:,5] = gw[:,:,:,2,2].copy()       # zz
+          #
+          g[START:END] = u.ravel().copy()
+          del gw, u
 
-          g[START:END][:,:,:,0] = gw[:,:,:,0,0]      # xx
-          g[START:END][:,:,:,1] = gw[:,:,:,0,1] *2.0 # xy
-          g[START:END][:,:,:,2] = gw[:,:,:,0,2] *2.0 # xz
-          g[START:END][:,:,:,3] = gw[:,:,:,1,1]      # yy
-          g[START:END][:,:,:,4] = gw[:,:,:,1,2] *2.0 # yz
-          g[START:END][:,:,:,5] = gw[:,:,:,2,2]      # zz
+          #g_xx = gw[:,:,:,0,0].ravel().copy()
+          #g_xy = gw[:,:,:,0,1].ravel().copy() * 2.0 
+          #g_xz = gw[:,:,:,0,2].ravel().copy() * 2.0
+          #g_yy = gw[:,:,:,1,1].ravel().copy()
+          #g_yz = gw[:,:,:,1,2].ravel().copy() * 2.0
+          #g_zz = gw[:,:,:,2,2].ravel().copy()
+
+          #g[START:END] = numpy.hstack([g_xx, g_xy, g_xz, g_yy, g_yz, g_zz])
+          #g[START:END][:,:,:,0] = g_xx.ravel()  # xx
+          #g[START:END][:,:,:,1] = g_xy.ravel()  # xy
+          #g[START:END][:,:,:,2] = g_xz.ravel()  # xz
+          #g[START:END][:,:,:,3] = g_yy.ravel()  # yy
+          #g[START:END][:,:,:,4] = g_yz.ravel()  # yz
+          #g[START:END][:,:,:,5] = g_zz.ravel()  # zz
 
       g *= -2.0
 
@@ -729,10 +786,10 @@ class Translation_DMSFit(DMSFit):
           N = PREV - DPREV
           #
           print(" * Computing Hessian (2,1) (2,1)...")
-          H_21_21 = numpy.einsum("bd,nac,niu,nix,njw,njy->abiuxcdjwy", self._A_AB_set,self._F_A_set,self._F_A_set,
+          H_21_21 = numpy.einsum("bd,nac,niu,nix,njw,njy->abiuxcdjwy", I,self._A_AB_set,self._F_A_set,self._F_A_set,
                                                                                       self._F_A_set,self._F_A_set)
 
-          H_21_21+= numpy.einsum("bd,nac,niu,nix,njw,njy->abiuxcdjwy", self._A_BA_set,self._F_B_set,self._F_B_set,
+          H_21_21+= numpy.einsum("bd,nac,niu,nix,njw,njy->abiuxcdjwy", I,self._A_BA_set,self._F_B_set,self._F_B_set,
                                                                                       self._F_B_set,self._F_B_set)
 
           H_21_21+= numpy.einsum("nda,nbc,niu,nix,njw,njy->abiuxcdjwy", self._W_AB_set,self._W_BA_set,
@@ -743,51 +800,94 @@ class Translation_DMSFit(DMSFit):
                                                                                       self._F_B_set,self._F_B_set,
                                                                                       self._F_A_set,self._F_A_set)
 
-          H[START:END,START:END][:,:,:,0,:,:,:,0] = H_21_21[:,:,:,0,0,:,:,:,0,0]       # xx,xx
-          H[START:END,START:END][:,:,:,0,:,:,:,1] = H_21_21[:,:,:,0,0,:,:,:,0,1] * 2.0 # xx,xy
-          H[START:END,START:END][:,:,:,0,:,:,:,2] = H_21_21[:,:,:,0,0,:,:,:,0,2] * 2.0 # xx,xz
-          H[START:END,START:END][:,:,:,0,:,:,:,3] = H_21_21[:,:,:,0,0,:,:,:,1,1]       # xx,yy
-          H[START:END,START:END][:,:,:,0,:,:,:,4] = H_21_21[:,:,:,0,0,:,:,:,1,2] * 2.0 # xx,yz
-          H[START:END,START:END][:,:,:,0,:,:,:,5] = H_21_21[:,:,:,0,0,:,:,:,2,2]       # xx,zz
+          U=numpy.zeros((self._dms.n,self._dms.n,self._dms.N,6,self._dms.n,self._dms.n,self._dms.N,6))
           #
-          H[START:END,START:END][:,:,:,1,:,:,:,0] = H_21_21[:,:,:,0,1,:,:,:,0,0] * 2.0 # xy,xx
-          H[START:END,START:END][:,:,:,1,:,:,:,1] = H_21_21[:,:,:,0,1,:,:,:,0,1] * 4.0 # xy,xy
-          H[START:END,START:END][:,:,:,1,:,:,:,2] = H_21_21[:,:,:,0,1,:,:,:,0,2] * 4.0 # xy,xz
-          H[START:END,START:END][:,:,:,1,:,:,:,3] = H_21_21[:,:,:,0,1,:,:,:,1,1] * 2.0 # xy,yy
-          H[START:END,START:END][:,:,:,1,:,:,:,4] = H_21_21[:,:,:,0,1,:,:,:,1,2] * 4.0 # xy,yz
-          H[START:END,START:END][:,:,:,1,:,:,:,5] = H_21_21[:,:,:,0,1,:,:,:,2,2] * 2.0 # xy,zz
+          U[:,:,:,0,:,:,:,0] = H_21_21[:,:,:,0,0,:,:,:,0,0].copy()       # xx,xx
+          U[:,:,:,0,:,:,:,1] = H_21_21[:,:,:,0,0,:,:,:,0,1].copy() * 2.0 # xx,xy
+          U[:,:,:,0,:,:,:,2] = H_21_21[:,:,:,0,0,:,:,:,0,2].copy() * 2.0 # xx,xz
+          U[:,:,:,0,:,:,:,3] = H_21_21[:,:,:,0,0,:,:,:,1,1].copy()       # xx,yy
+          U[:,:,:,0,:,:,:,4] = H_21_21[:,:,:,0,0,:,:,:,1,2].copy() * 2.0 # xx,yz
+          U[:,:,:,0,:,:,:,5] = H_21_21[:,:,:,0,0,:,:,:,2,2].copy()       # xx,zz
           #
-          H[START:END,START:END][:,:,:,2,:,:,:,0] = H_21_21[:,:,:,0,2,:,:,:,0,0] * 2.0 # xz,xx
-          H[START:END,START:END][:,:,:,2,:,:,:,1] = H_21_21[:,:,:,0,2,:,:,:,0,1] * 4.0 # xz,xy
-          H[START:END,START:END][:,:,:,2,:,:,:,2] = H_21_21[:,:,:,0,2,:,:,:,0,2] * 4.0 # xz,xz
-          H[START:END,START:END][:,:,:,2,:,:,:,3] = H_21_21[:,:,:,0,2,:,:,:,1,1] * 2.0 # xz,yy
-          H[START:END,START:END][:,:,:,2,:,:,:,4] = H_21_21[:,:,:,0,2,:,:,:,1,2] * 4.0 # xz,yz
-          H[START:END,START:END][:,:,:,2,:,:,:,5] = H_21_21[:,:,:,0,2,:,:,:,2,2] * 2.0 # xz,zz
+          U[:,:,:,1,:,:,:,0] = H_21_21[:,:,:,0,1,:,:,:,0,0].copy() * 2.0 # xy,xx
+          U[:,:,:,1,:,:,:,1] = H_21_21[:,:,:,0,1,:,:,:,0,1].copy() * 4.0 # xy,xy
+          U[:,:,:,1,:,:,:,2] = H_21_21[:,:,:,0,1,:,:,:,0,2].copy() * 4.0 # xy,xz
+          U[:,:,:,1,:,:,:,3] = H_21_21[:,:,:,0,1,:,:,:,1,1].copy() * 2.0 # xy,yy
+          U[:,:,:,1,:,:,:,4] = H_21_21[:,:,:,0,1,:,:,:,1,2].copy() * 4.0 # xy,yz
+          U[:,:,:,1,:,:,:,5] = H_21_21[:,:,:,0,1,:,:,:,2,2].copy() * 2.0 # xy,zz
           #
-          H[START:END,START:END][:,:,:,3,:,:,:,0] = H_21_21[:,:,:,1,1,:,:,:,0,0]       # yy,xx
-          H[START:END,START:END][:,:,:,3,:,:,:,1] = H_21_21[:,:,:,1,1,:,:,:,0,1] * 2.0 # yy,xy
-          H[START:END,START:END][:,:,:,3,:,:,:,2] = H_21_21[:,:,:,1,1,:,:,:,0,2] * 2.0 # yy,xz
-          H[START:END,START:END][:,:,:,3,:,:,:,3] = H_21_21[:,:,:,1,1,:,:,:,1,1]       # yy,yy
-          H[START:END,START:END][:,:,:,3,:,:,:,4] = H_21_21[:,:,:,1,1,:,:,:,1,2] * 2.0 # yy,yz
-          H[START:END,START:END][:,:,:,3,:,:,:,5] = H_21_21[:,:,:,1,1,:,:,:,2,2]       # yy,zz
+          U[:,:,:,2,:,:,:,0] = H_21_21[:,:,:,0,2,:,:,:,0,0].copy() * 2.0 # xz,xx
+          U[:,:,:,2,:,:,:,1] = H_21_21[:,:,:,0,2,:,:,:,0,1].copy() * 4.0 # xz,xy
+          U[:,:,:,2,:,:,:,2] = H_21_21[:,:,:,0,2,:,:,:,0,2].copy() * 4.0 # xz,xz
+          U[:,:,:,2,:,:,:,3] = H_21_21[:,:,:,0,2,:,:,:,1,1].copy() * 2.0 # xz,yy
+          U[:,:,:,2,:,:,:,4] = H_21_21[:,:,:,0,2,:,:,:,1,2].copy() * 4.0 # xz,yz
+          U[:,:,:,2,:,:,:,5] = H_21_21[:,:,:,0,2,:,:,:,2,2].copy() * 2.0 # xz,zz
           #
-          H[START:END,START:END][:,:,:,4,:,:,:,0] = H_21_21[:,:,:,1,2,:,:,:,0,0] * 2.0 # yz,xx
-          H[START:END,START:END][:,:,:,4,:,:,:,1] = H_21_21[:,:,:,1,2,:,:,:,0,1] * 4.0 # yz,xy
-          H[START:END,START:END][:,:,:,4,:,:,:,2] = H_21_21[:,:,:,1,2,:,:,:,0,2] * 4.0 # yz,xz
-          H[START:END,START:END][:,:,:,4,:,:,:,3] = H_21_21[:,:,:,1,2,:,:,:,1,1] * 2.0 # yz,yy
-          H[START:END,START:END][:,:,:,4,:,:,:,4] = H_21_21[:,:,:,1,2,:,:,:,1,2] * 4.0 # yz,yz
-          H[START:END,START:END][:,:,:,4,:,:,:,5] = H_21_21[:,:,:,1,2,:,:,:,2,2] * 2.0 # yz,zz
+          U[:,:,:,3,:,:,:,0] = H_21_21[:,:,:,1,1,:,:,:,0,0].copy()       # yy,xx
+          U[:,:,:,3,:,:,:,1] = H_21_21[:,:,:,1,1,:,:,:,0,1].copy() * 2.0 # yy,xy
+          U[:,:,:,3,:,:,:,2] = H_21_21[:,:,:,1,1,:,:,:,0,2].copy() * 2.0 # yy,xz
+          U[:,:,:,3,:,:,:,3] = H_21_21[:,:,:,1,1,:,:,:,1,1].copy()       # yy,yy
+          U[:,:,:,3,:,:,:,4] = H_21_21[:,:,:,1,1,:,:,:,1,2].copy() * 2.0 # yy,yz
+          U[:,:,:,3,:,:,:,5] = H_21_21[:,:,:,1,1,:,:,:,2,2].copy()       # yy,zz
           #
-          H[START:END,START:END][:,:,:,5,:,:,:,0] = H_21_21[:,:,:,2,2,:,:,:,0,0]       # zz,xx
-          H[START:END,START:END][:,:,:,5,:,:,:,1] = H_21_21[:,:,:,2,2,:,:,:,0,1] * 2.0 # zz,xy
-          H[START:END,START:END][:,:,:,5,:,:,:,2] = H_21_21[:,:,:,2,2,:,:,:,0,2] * 2.0 # zz,xz
-          H[START:END,START:END][:,:,:,5,:,:,:,3] = H_21_21[:,:,:,2,2,:,:,:,1,1]       # zz,yy
-          H[START:END,START:END][:,:,:,5,:,:,:,4] = H_21_21[:,:,:,2,2,:,:,:,1,2] * 2.0 # zz,yz
-          H[START:END,START:END][:,:,:,5,:,:,:,5] = H_21_21[:,:,:,2,2,:,:,:,2,2]       # zz,zz
+          U[:,:,:,4,:,:,:,0] = H_21_21[:,:,:,1,2,:,:,:,0,0].copy() * 2.0 # yz,xx
+          U[:,:,:,4,:,:,:,1] = H_21_21[:,:,:,1,2,:,:,:,0,1].copy() * 4.0 # yz,xy
+          U[:,:,:,4,:,:,:,2] = H_21_21[:,:,:,1,2,:,:,:,0,2].copy() * 4.0 # yz,xz
+          U[:,:,:,4,:,:,:,3] = H_21_21[:,:,:,1,2,:,:,:,1,1].copy() * 2.0 # yz,yy
+          U[:,:,:,4,:,:,:,4] = H_21_21[:,:,:,1,2,:,:,:,1,2].copy() * 4.0 # yz,yz
+          U[:,:,:,4,:,:,:,5] = H_21_21[:,:,:,1,2,:,:,:,2,2].copy() * 2.0 # yz,zz
+          #
+          U[:,:,:,5,:,:,:,0] = H_21_21[:,:,:,2,2,:,:,:,0,0].copy()       # zz,xx
+          U[:,:,:,5,:,:,:,1] = H_21_21[:,:,:,2,2,:,:,:,0,1].copy() * 2.0 # zz,xy
+          U[:,:,:,5,:,:,:,2] = H_21_21[:,:,:,2,2,:,:,:,0,2].copy() * 2.0 # zz,xz
+          U[:,:,:,5,:,:,:,3] = H_21_21[:,:,:,2,2,:,:,:,1,1].copy()       # zz,yy
+          U[:,:,:,5,:,:,:,4] = H_21_21[:,:,:,2,2,:,:,:,1,2].copy() * 2.0 # zz,yz
+          U[:,:,:,5,:,:,:,5] = H_21_21[:,:,:,2,2,:,:,:,2,2].copy()       # zz,zz
           # 
-          del H_21_21
+          H[START:END,START:END] = U.reshape(L,L).copy()
+          del H_21_21, U
 
+          # (01,21)
+          print(" * Computing Hessian (0,1) (2,1)...")
+          H_01_21 = numpy.einsum("bd,nac,njw,njx->abcdjwx", I,self._A_AB_set,self._F_A_set,self._F_A_set)
+          H_01_21+= numpy.einsum("bd,nac,njw,njx->abcdjwx", I,self._A_BA_set,self._F_B_set,self._F_B_set)
+          H_01_21+= numpy.einsum("nda,nbc,njw,njx->abcdjwx", self._W_AB_set,self._W_BA_set,self._F_B_set,self._F_B_set)
+          H_01_21+= numpy.einsum("nda,nbc,njw,njx->abcdjwx", self._W_BA_set,self._W_AB_set,self._F_A_set,self._F_A_set)
+          #
+          U = numpy.zeros((self._dms.n,self._dms.n,self._dms.n,self._dms.n,self._dms.N,6))
+          #
+          U[:,:,:,:,:,0] = H_01_21[:,:,:,:,:,0,0].copy()       # xx
+          U[:,:,:,:,:,1] = H_01_21[:,:,:,:,:,0,1].copy() * 2.0 # xy
+          U[:,:,:,:,:,2] = H_01_21[:,:,:,:,:,0,2].copy() * 2.0 # xz
+          U[:,:,:,:,:,3] = H_01_21[:,:,:,:,:,1,1].copy()       # yy
+          U[:,:,:,:,:,4] = H_01_21[:,:,:,:,:,1,2].copy() * 2.0 # yz
+          U[:,:,:,:,:,5] = H_01_21[:,:,:,:,:,2,2].copy()       # zz
+          #
+          H[DPREV:PREV,START:END] = U.reshape(N,L).copy()
+          del H_01_21, U
+          H[START:END,DPREV:PREV] = H[DPREV:PREV,START:END].T.copy()
 
+          # (11,21)
+          print(" * Computing Hessian (1,1) (2,1)...")
+          H_11_21 = numpy.einsum("bd,nac,nix,nju,njw->abixcdjuw", I,self._A_AB_set,self._F_A_set,self._F_A_set,self._F_A_set)
+          H_11_21+= numpy.einsum("bd,nac,nix,nju,njw->abixcdjuw", I,self._A_BA_set,self._F_B_set,self._F_B_set,self._F_B_set)
+          H_11_21+= numpy.einsum("nda,nbc,nix,nju,njw->abixcdjuw",self._W_AB_set,self._W_BA_set,
+                                                                  self._F_A_set,self._F_B_set,self._F_B_set)
+          H_11_21+= numpy.einsum("nda,nbc,nix,nju,njw->abixcdjuw",self._W_BA_set,self._W_AB_set,
+                                                                  self._F_B_set,self._F_A_set,self._F_A_set)
+          #
+          U = numpy.zeros((self._dms.n,self._dms.n,self._dms.N,3,self._dms.n,self._dms.n,self._dms.N,6))
+          #
+          U[:,:,:,:,:,:,:,0] = H_11_21[:,:,:,:,:,:,:,0,0].copy()       # xx
+          U[:,:,:,:,:,:,:,1] = H_11_21[:,:,:,:,:,:,:,0,1].copy() * 2.0 # xy
+          U[:,:,:,:,:,:,:,2] = H_11_21[:,:,:,:,:,:,:,0,2].copy() * 2.0 # xz
+          U[:,:,:,:,:,:,:,3] = H_11_21[:,:,:,:,:,:,:,1,1].copy()       # yy
+          U[:,:,:,:,:,:,:,4] = H_11_21[:,:,:,:,:,:,:,1,2].copy() * 2.0 # yz
+          U[:,:,:,:,:,:,:,5] = H_11_21[:,:,:,:,:,:,:,2,2].copy()       # zz
+          # 
+          H[PREV:START,START:END] = U.reshape(M,L).copy()
+          del H_11_21, U
+          H[START:END,PREV:START] = H[PREV:START,START:END].T.copy()
 
 
       H *= 2.0
@@ -802,7 +902,7 @@ class Translation_DMSFit(DMSFit):
       # extract susceptibilities
       print(" * Setting the DMS tensors...")
       self._dms.set_s2(s)
-      for order in [(0,1),(1,1),]:
+      for order in [(0,1),(1,1),(2,1)]:
           if order in self._dms.available_orders():
              self._dms._generate_B_from_s(order)
 
@@ -833,10 +933,14 @@ class Translation_DMSFit(DMSFit):
           
           W_AB = self._W_AB_set[s]
           W_BA = self._W_BA_set[s]
+         #w_AB = self._w_AB_set[s]
+         #w_BA = self._w_BA_set[s]
          #Wf_AB= self._Wf_AB_set[s]
          #Wf_BA= self._Wf_BA_set[s]
           F_A  = self._F_A_set[s]
           F_B  = self._F_B_set[s]
+
+          H_AB_ref = self._H_AB_set_ref[s]
 
           dD_AB_ref = self._dD_AB_set_ref[s]
           dD_AB_com = W_AB @ B_01 + B_01.T @ W_BA.T
@@ -845,10 +949,17 @@ class Translation_DMSFit(DMSFit):
           if (1,1) in self._dms.available_orders():
               dD_AB_com+= numpy.einsum("ac,ix,cbix->ab",W_AB, F_A, B_11)
               dD_AB_com+= numpy.einsum("ac,ix,cbix->ab",W_BA, F_B, B_11).T
+          if (2,1) in self._dms.available_orders():
+              dD_AB_com+= numpy.einsum("ac,ix,iy,cbixy->ab",W_AB, F_A, F_A, B_21)
+              dD_AB_com+= numpy.einsum("ac,ix,iy,cbixy->ab",W_BA, F_B, F_B, B_21).T
 
           rms = self._rms(dD_AB_ref, dD_AB_com)
 
-          print(" Sample=%03d  RMS=%13.5E  %14.5f %14.5f" % (s+1, rms, dD_AB_ref[0,0], dD_AB_com[0,0]))
+          r = (dD_AB_ref @ H_AB_ref).trace()
+          c = (dD_AB_com @ H_AB_ref).trace()
+
+          print(" Sample=%03d  RMS=%13.5E  %14.5f %14.5f  Tr[dD,H]=%14.5f  %14.5f" \
+                   % (s+1, rms, dD_AB_ref[0,0], dD_AB_com[0,0], r, c))
 
           self._i = s
           self._save_dD(dD_AB_ref, prefix='ref')
