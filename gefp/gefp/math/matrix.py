24540e6cb54f06f36ff7c3224591c47b3fce5761
globulion <blasiak.bartosz@gmail.com>
2020-06-10 13:28:39 +0200

    Rotation of 6D and 10F functions implemented

--- gefp/gefp/math/matrix.py
+++ gefp/gefp/math/matrix.py
@@ -166,6 +166,9 @@ def make_r2(r):
     R6 = numpy.zeros((6,6))
     #s = math.sqrt(3.0) / 2.0
     s = 1.0
+    # code derived manually
+    # code compatible with rot_6d.py after transposition of final matrix
+
     # for 0 - XX
     R6[0,0] = r[0,0] * r[0,0]                            # XX XX
     R6[0,1] = r[0,0] * r[1,0] * 2.0 * s                  # XX XY
@@ -209,26 +212,117 @@ def make_r2(r):
     R6[5,4] = r[1,2] * r[2,2] * 2.0 * s                  # ZZ YZ
     R6[5,5] = r[2,2] * r[2,2]                            # ZZ ZZ
 
-    #sc = numpy.identity(6, numpy.float64)
-    #s = math.sqrt(3.0) / 2.0
-    #sc[1,1] = s
-    #sc[2,2] = s
-    #sc[4,4] = s
-    #sci = numpy.linalg.inv(sc)
-    #R6 = numpy.linalg.multi_dot([sci, R6, sc])
     return R6.T
 
 def make_r3(r):
     "Create 10 by 10 transformation matrix for 10F-type vector elements"
     R10 = numpy.zeros((10,10))
-    s = 1.0
     r00 = r[0,0]; r11 = r[1,1]; r22 = r[2,2]
     r01 = r[0,1]; r02 = r[0,2]; r12 = r[1,2]
     r10 = r[1,0]; r20 = r[2,0]; r21 = r[2,1]
-    # for 0 - xxx
-    #TODO
-    R10[0,0] = r00 * r00 * r00                            # xxx xxx
-    return R10.T
+    # code generated from rot_10f.py
+    R10[0,0] = r00*r00*r00                                                                       
+    R10[0,1] = r00*r00*r01
+    R10[0,2] = r00*r00*r02
+    R10[0,3] = r00*r01*r01
+    R10[0,4] = r00*r01*r02
+    R10[0,5] = r00*r02*r02
+    R10[0,6] = r01*r01*r01
+    R10[0,7] = r01*r01*r02
+    R10[0,8] = r01*r02*r02
+    R10[0,9] = r02*r02*r02
+    R10[1,0] = r10*r00*r00 + r00*r10*r00 + r00*r00*r10
+    R10[1,1] = r10*r00*r01 + r00*r10*r01 + r00*r00*r11
+    R10[1,2] = r10*r00*r02 + r00*r10*r02 + r00*r00*r12
+    R10[1,3] = r10*r01*r01 + r00*r11*r01 + r00*r01*r11
+    R10[1,4] = r10*r01*r02 + r00*r11*r02 + r00*r01*r12
+    R10[1,5] = r10*r02*r02 + r00*r12*r02 + r00*r02*r12
+    R10[1,6] = r11*r01*r01 + r01*r11*r01 + r01*r01*r11
+    R10[1,7] = r11*r01*r02 + r01*r11*r02 + r01*r01*r12
+    R10[1,8] = r11*r02*r02 + r01*r12*r02 + r01*r02*r12
+    R10[1,9] = r12*r02*r02 + r02*r12*r02 + r02*r02*r12
+    R10[2,0] = r20*r00*r00 + r00*r20*r00 + r00*r00*r20
+    R10[2,1] = r20*r00*r01 + r00*r20*r01 + r00*r00*r21
+    R10[2,2] = r20*r00*r02 + r00*r20*r02 + r00*r00*r22
+    R10[2,3] = r20*r01*r01 + r00*r21*r01 + r00*r01*r21
+    R10[2,4] = r20*r01*r02 + r00*r21*r02 + r00*r01*r22
+    R10[2,5] = r20*r02*r02 + r00*r22*r02 + r00*r02*r22
+    R10[2,6] = r21*r01*r01 + r01*r21*r01 + r01*r01*r21
+    R10[2,7] = r21*r01*r02 + r01*r21*r02 + r01*r01*r22
+    R10[2,8] = r21*r02*r02 + r01*r22*r02 + r01*r02*r22
+    R10[2,9] = r22*r02*r02 + r02*r22*r02 + r02*r02*r22
+    R10[3,0] = r00*r10*r10 + r10*r00*r10 + r10*r10*r00
+    R10[3,1] = r00*r10*r11 + r10*r00*r11 + r10*r10*r01
+    R10[3,2] = r00*r10*r12 + r10*r00*r12 + r10*r10*r02
+    R10[3,3] = r00*r11*r11 + r10*r01*r11 + r10*r11*r01
+    R10[3,4] = r00*r11*r12 + r10*r01*r12 + r10*r11*r02
+    R10[3,5] = r00*r12*r12 + r10*r02*r12 + r10*r12*r02
+    R10[3,6] = r01*r11*r11 + r11*r01*r11 + r11*r11*r01
+    R10[3,7] = r01*r11*r12 + r11*r01*r12 + r11*r11*r02
+    R10[3,8] = r01*r12*r12 + r11*r02*r12 + r11*r12*r02
+    R10[3,9] = r02*r12*r12 + r12*r02*r12 + r12*r12*r02
+    R10[4,0] = r00*r10*r20 + r00*r20*r10 + r10*r00*r20 + r10*r20*r00 + r20*r00*r10 + r20*r10*r00
+    R10[4,1] = r00*r10*r21 + r00*r20*r11 + r10*r00*r21 + r10*r20*r01 + r20*r00*r11 + r20*r10*r01
+    R10[4,2] = r00*r10*r22 + r00*r20*r12 + r10*r00*r22 + r10*r20*r02 + r20*r00*r12 + r20*r10*r02
+    R10[4,3] = r00*r11*r21 + r00*r21*r11 + r10*r01*r21 + r10*r21*r01 + r20*r01*r11 + r20*r11*r01
+    R10[4,4] = r00*r11*r22 + r00*r21*r12 + r10*r01*r22 + r10*r21*r02 + r20*r01*r12 + r20*r11*r02
+    R10[4,5] = r00*r12*r22 + r00*r22*r12 + r10*r02*r22 + r10*r22*r02 + r20*r02*r12 + r20*r12*r02
+    R10[4,6] = r01*r11*r21 + r01*r21*r11 + r11*r01*r21 + r11*r21*r01 + r21*r01*r11 + r21*r11*r01
+    R10[4,7] = r01*r11*r22 + r01*r21*r12 + r11*r01*r22 + r11*r21*r02 + r21*r01*r12 + r21*r11*r02
+    R10[4,8] = r01*r12*r22 + r01*r22*r12 + r11*r02*r22 + r11*r22*r02 + r21*r02*r12 + r21*r12*r02
+    R10[4,9] = r02*r12*r22 + r02*r22*r12 + r12*r02*r22 + r12*r22*r02 + r22*r02*r12 + r22*r12*r02
+    R10[5,0] = r00*r20*r20 + r20*r00*r20 + r20*r20*r00
+    R10[5,1] = r00*r20*r21 + r20*r00*r21 + r20*r20*r01
+    R10[5,2] = r00*r20*r22 + r20*r00*r22 + r20*r20*r02
+    R10[5,3] = r00*r21*r21 + r20*r01*r21 + r20*r21*r01
+    R10[5,4] = r00*r21*r22 + r20*r01*r22 + r20*r21*r02
+    R10[5,5] = r00*r22*r22 + r20*r02*r22 + r20*r22*r02
+    R10[5,6] = r01*r21*r21 + r21*r01*r21 + r21*r21*r01
+    R10[5,7] = r01*r21*r22 + r21*r01*r22 + r21*r21*r02
+    R10[5,8] = r01*r22*r22 + r21*r02*r22 + r21*r22*r02
+    R10[5,9] = r02*r22*r22 + r22*r02*r22 + r22*r22*r02
+    R10[6,0] = r10*r10*r10
+    R10[6,1] = r10*r10*r11
+    R10[6,2] = r10*r10*r12
+    R10[6,3] = r10*r11*r11
+    R10[6,4] = r10*r11*r12
+    R10[6,5] = r10*r12*r12
+    R10[6,6] = r11*r11*r11
+    R10[6,7] = r11*r11*r12
+    R10[6,8] = r11*r12*r12
+    R10[6,9] = r12*r12*r12
+    R10[7,0] = r20*r10*r10 + r10*r20*r10 + r10*r10*r20
+    R10[7,1] = r20*r10*r11 + r10*r20*r11 + r10*r10*r21
+    R10[7,2] = r20*r10*r12 + r10*r20*r12 + r10*r10*r22
+    R10[7,3] = r20*r11*r11 + r10*r21*r11 + r10*r11*r21
+    R10[7,4] = r20*r11*r12 + r10*r21*r12 + r10*r11*r22
+    R10[7,5] = r20*r12*r12 + r10*r22*r12 + r10*r12*r22
+    R10[7,6] = r21*r11*r11 + r11*r21*r11 + r11*r11*r21
+    R10[7,7] = r21*r11*r12 + r11*r21*r12 + r11*r11*r22
+    R10[7,8] = r21*r12*r12 + r11*r22*r12 + r11*r12*r22
+    R10[7,9] = r22*r12*r12 + r12*r22*r12 + r12*r12*r22
+    R10[8,0] = r10*r20*r20 + r20*r10*r20 + r20*r20*r10
+    R10[8,1] = r10*r20*r21 + r20*r10*r21 + r20*r20*r11
+    R10[8,2] = r10*r20*r22 + r20*r10*r22 + r20*r20*r12
+    R10[8,3] = r10*r21*r21 + r20*r11*r21 + r20*r21*r11
+    R10[8,4] = r10*r21*r22 + r20*r11*r22 + r20*r21*r12
+    R10[8,5] = r10*r22*r22 + r20*r12*r22 + r20*r22*r12
+    R10[8,6] = r11*r21*r21 + r21*r11*r21 + r21*r21*r11
+    R10[8,7] = r11*r21*r22 + r21*r11*r22 + r21*r21*r12
+    R10[8,8] = r11*r22*r22 + r21*r12*r22 + r21*r22*r12
+    R10[8,9] = r12*r22*r22 + r22*r12*r22 + r22*r22*r12
+    R10[9,0] = r20*r20*r20
+    R10[9,1] = r20*r20*r21
+    R10[9,2] = r20*r20*r22
+    R10[9,3] = r20*r21*r21
+    R10[9,4] = r20*r21*r22
+    R10[9,5] = r20*r22*r22
+    R10[9,6] = r21*r21*r21
+    R10[9,7] = r21*r21*r22
+    R10[9,8] = r21*r22*r22
+    R10[9,9] = r22*r22*r22
+
+    return R10
 
 
 def rotate_ao_matrix(M, rot_3d, bfs, return_rot=False, aomo=False):
@@ -243,6 +337,8 @@ def rotate_ao_matrix(M, rot_3d, bfs, return_rot=False, aomo=False):
  Returns: 
      rotated matrix M:  M' = R.T @ M @ R (if aomo=False)
                     M:  M' = R.T @ M     (if aomo=True)
+
+ Notes: rotation up to 10F functions is implemented.
 """
     # number of basis functions per shell type
     if bfs.has_puream():  nam = {0: 1, 1: 3, 2: 5, 3: 7, 4: 9}
@@ -319,7 +415,10 @@ def rotate_ao_matrix(M, rot_3d, bfs, return_rot=False, aomo=False):
     if max_am > 1: populate_R(2, make_r2(rot_3d.copy()))
 
     # --- f block
-    if max_am > 2: raise NotImplementedError
+    if max_am > 2: populate_R(3, make_r3(rot_3d.copy()))
+
+    # --- g block
+    if max_am > 3: raise NotImplementedError
 
     # rotate
     if aomo == True:
